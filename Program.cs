6e0c737f70be69c595c7c456109006ccba91b769
ikezawa67 <ikezawa67@gmail.com>
2023-03-17 16:18:00 +0900

    version 0.0.3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
-﻿namespace GeneticAlgorithm
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace GeneticAlgorithm
 {
+    public class Globals
+    {
+        public double[] args;
+
+        public Globals(double[] args)
+        {
+            this.args = args;
+        }
+    }
+
     class Program
     {
-        static double fitness(double[] args)
-        {
-            double x = args[0];
-            double y = args[1];
-            return x * y;
-        }
 
         public static void Main()
         {
-            GeneticAlgorithm ga = new GeneticAlgorithm(50, 10, 2, fitness, SelectionMethod.Tournament, CrossoverMethod.OnePoint, 0.1, 0.01, 2);
-            Console.WriteLine("Max: {0}, Min: {1}", ga.MaxIndividual, ga.MinIndividual);
-            for (int i = 1; i <= 100; i++)
-            {
-                ga.Step();
-            }
+            Script<double> script = CSharpScript.Create<double>(@"
+                return args[0] * args[1];
+            ", globalsType: typeof(Globals));
+            Globals globals = new Globals(new double[] { 2, 2 });
+            var result = script.RunAsync(globals).Result;
+            Console.Write(result.ReturnValue);
+            // GeneticAlgorithm ga = new GeneticAlgorithm(100, 10, 2, fitness, SelectionMethod.Ranking, CrossoverMethod.Uniform, 0.1, 0.01, 2);
+            // ga.Execution(1000);
         }
     }
 }
